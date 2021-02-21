2bbf5cc276a78beab6e337a8e2fd213fb7fee1c1
yokoe <you@example.com>
2021-02-21 18:19:14 +0900

    image fieldをレシピモデルに追加

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from django.test import TestCase
 
 from django.contrib.auth import get_user_model
@@ -82,3 +84,14 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    @patch('uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        '''画像が正しい場所に保存されるかテスト
+        '''
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'myimage.jpg')
+
+        exp_path = f'uploads/recipe/{uuid}.jpg'
+        self.assertEqual(file_path, exp_path)
