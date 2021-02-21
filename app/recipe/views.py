dfbc77324d8a99e6f39199649fd7b50c526c3fd4
yokoe <you@example.com>
2021-02-21 17:21:44 +0900

    レシピ作成機能追加

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -58,3 +58,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
         if self.action == 'retrieve':
             return serializers.RecipeDetailSerializer
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        '''レシピ新規登録
+        '''
+        serializer.save(user=self.request.user)
